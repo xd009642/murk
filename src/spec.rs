0a66e4181c2acc4f46c2cffee40aa57ac7ebaca2
xd009642 <danielmckenna93@gmail.com>
2021-03-22 22:57:43 +0400

    Add spec and a test for it

--- src/spec.rs
+++ src/spec.rs
@@ -34,8 +34,10 @@ pub struct PathItem {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Operation {
+    #[serde(default)]
     request_data: IndexMap<String, Data>,
     request_body: RequestBody,
+    #[serde(default)]
     parameters: Vec<Parameter>,
     #[serde(default = "one")]
     weight: usize,
@@ -44,8 +46,50 @@ pub struct Operation {
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Data {
-    value: Option<Value>,
-    external_value: Option<PathBuf>,
+    values: IndexMap<String, TestValue>,
     #[serde(default = "one")]
     weight: usize,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TestValue {
+    value: Option<Value>,
+    external_value: Option<PathBuf>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_yaml::from_str;
+
+    #[test]
+    fn deserialise_specification() {
+        let sample_spec = r#"
+            paths:
+              upload:
+                post:
+                  requestData:
+                    static_string:
+                      values:
+                        file: 
+                          value: "I am a files contents"
+                    file_upload:
+                      values:
+                        file:
+                          external_value: "/home/xd009642/corpus"
+                  requestBody:
+                    content:
+                      multipart/form-data:
+                        schema: 
+                          type: object
+                          properties:
+                            file:
+                              type: string
+                              format: binary
+
+        "#;
+
+        let _spec: Specification = from_str(sample_spec).unwrap();
+    }
 }
